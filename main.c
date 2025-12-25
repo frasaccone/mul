d6b0bec46fd5ee522229ee319c3d668f3aaa9525
Francesco Saccone <francesco@francescosaccone.com>
2025-12-25 20:01:45 +0100

    fix(main.c): call fclose before returning

--- main.c
+++ main.c
@@ -56,11 +56,15 @@ main(int argc, char **argv)
 	fseek(file, 0, SEEK_SET);
 
 	if (!(in = malloc(insize + 1))) {
+		if (file != stdin)
+			fclose(file);
 		perror("malloc");
 		return EXIT_FAILURE;
 	}
 
 	if (fread(in, insize, 1, file) < 1) {
+		if (file != stdin)
+			fclose(file);
 		perror("fread");
 		return EXIT_FAILURE;
 	}
